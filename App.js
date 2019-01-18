48a1ef6f061572c4ac0cf31cae8ce545d8430a5f
linnnruoo <linruo2014@gmail.com>
2019-01-18 22:06:51 +0800

    add ar car demo

--- App.js
+++ App.js
@@ -30,7 +30,7 @@ var sharedProps = {
 }
 
 // Sets the default scene you want for AR and VR
-var InitialARScene = require('./js/HelloWorldSceneAR');
+var InitialARScene = require('./js/ARCarDemo/ARCarDemo');
 var InitialVRScene = require('./js/HelloWorldScene');
 
 var UNSET = "UNSET";
